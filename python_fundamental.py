2ed1122e5a187cd6f82d876d855267624034f3d9
Maruf khan <2020pgicsmaruf043@poorniama.org>
2023-07-22 09:59:12 +0530

    first commit

---
+++ python_fundamental.py
@@ -0,0 +1,16 @@
+# Brackets
+# identifiers
+# operators
+# data & types
+# build-in functions for data types
+# control statements
+# Conditional 
+# Looping
+# Comprehensions
+# User defined functions
+# Function Parameterization
+# Lambda Expressions
+# git init
+# User configuration
+#   git config --global user.name "Maruf khan"
+#   git config --global user.email "2020pgics043poornima.org"
