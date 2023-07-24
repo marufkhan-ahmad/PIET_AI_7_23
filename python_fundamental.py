fc5a7ab090211089372b95dc73052253f605589b
Maruf khan <2020pgicsmaruf043@poorniama.org>
2023-07-24 10:39:20 +0530

    Data types

--- python_fundamental.py
+++ python_fundamental.py
@@ -1,7 +1,28 @@
-# Brackets
-# identifiers
-# operators
-# data & types
+# Brackets three brackets () :=> Args, Expressions, Tuplt, {} :=> dict, set, []:=> indexing 
+# identifiers * it must contain alpha numeric only, *special char not allowed except Underscore, * cna not start from number
+# operators 1. Arithmatic : +, -, *, / , %, **, //(floor div), 2.Assignment: =, +=,-=, ........
+# 3. Relational : >,<,>=,<=,==, !=, 4.Logical : and ,or ,not, 5. Bitwise: &, |,~,^(xor),>>,<<
+# 6.Membership: in, not in, 7. Identity: is , is not
+# Keywords
+# data & types 
+# int, 10
+# float,10.90
+# complex, 10+10j
+# str, ' ', "",""" """
+# list,unstructured mutable collection of het data
+# tuple,unstructured mutable collection of het data
+# dict,structured mutable collection of key-value pairs
+# set, unordered collection of unique items
+# bool,True , False
+# NoneType    None
+# Concept of Mutability
+# Mutable and Immutable 
+# and it will work upon collection or sequences
+# 1.Item Access:=> Indexing, Slicing, Sub-scripting
+# 2.Item Insertion
+# 3.Item Assignment
+# 4.Item Deletion
+# Immutable :=> (1),(2),Possible but not the (3),and (4)
 # build-in functions for data types
 # control statements
 # Conditional 
